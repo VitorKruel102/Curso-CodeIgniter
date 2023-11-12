1818c24b2f0a0a8cdcb3a92c4ec687cb45ca0fc4
Vitor Kruel <93736784+VitorKruel102@users.noreply.github.com>
2023-11-12 17:33:54 -0300

    Entendendo sobre a pasta Libraries;

--- Projetos/Projeto001/app/Controllers/Loja/Venda.php
+++ Projetos/Projeto001/app/Controllers/Loja/Venda.php
@@ -1,13 +1,19 @@
 <?php namespace App\Controllers\Loja;
 
+use App\Libraries\Loja\Venda;
 use CodeIgniter\Controller;
+
 
 class Vendas extends Controller {
     protected $helpers = array('date', 'matematica');
 
     public function index() {
         $data["frase"] = adicionar(1, 5);
-        
+
+        $v = new Venda();
+        $v->produto = 'Automovel';
+        $v->preco = '100';
+
         return view("pagina1", $data);
     }
 }
---
+++ Projetos/Projeto001/app/Libraries/Loja/Venda.php
@@ -0,0 +1,12 @@
+<?php 
+
+namespace App\Libraries\Loja;
+
+class Venda {
+    public  $produto;
+    public  $preco;
+
+    public function __construct() {
+
+    }
+}
